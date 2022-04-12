92c4af9fba08f3f7c0fc38cca1dfc250521b6faa
GrantHS <6nohx1@gmail.com>
2022-04-12 10:38:15 -0700

    Imported Maze File
    doors 2 assignment references

---
+++ Group22 Game Project 180/Assets/Scripts/CharController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+/*
+ * 
+ * DO NOT! edit this script create your own and add it as a compenant to this game object 
+ * 
+ * 
+ * You may look at this script and try to understand it if you want some extra practice reading code though :D
+ */
+ 
+public class CharController : MonoBehaviour {
+
+    //A boolean value that will tell you if you are within .1 Unity Unit from the ground
+    private bool is_grounded;
+    //The rigid body attached to the player
+    private Rigidbody body;
+    //how close are we to a wall on our left side
+    private float distance_to_wall_left = 2f;
+    //how close are we to a wall on our right side
+    private float distance_to_wall_right = 2f;
+    //how close are we to a wall going forward
+    private float distance_to_wall_forward = 2f;
+    //how close are we to a wall going backwards
+    private float distance_to_wall_back = 2f;
+
+    //Start is a function that is called once when the object is Instatiated. 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        body = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is a function that is called once per frame
+    void Update()
+    {
+
+        float translation = Input.GetAxis("Vertical") * 10 * Time.deltaTime;
+        float straffe = Input.GetAxis("Horizontal") * 10 * Time.deltaTime;
+
+        if (!is_grounded)
+            straffe /= 2;
+
+        if (!is_grounded)
+            translation /= 2;
+
+
+        //if I'm too close to a wall, don't go that direction anymore
+        if ((distance_to_wall_back <= .6 && translation < 0) || (distance_to_wall_forward <= .6 && translation > 0))
+        {
+            translation = 0;
+        }
+        if ((distance_to_wall_right < .6 && straffe > 0) || (distance_to_wall_left < .6 && straffe < 0))
+        {
+            straffe = 0;
+        }
+
+        //Translate to move.
+        transform.Translate(straffe, 0, translation);
+
+
+
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        DistanceToWall();
+        Vector3 oldRot = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0, oldRot.y, 0);
+    }
+
+    private void DistanceToWall()
+    {
+        RaycastHit hit;
+        Ray left_ray = new Ray(transform.position, -transform.right);
+        Ray front_ray = new Ray(transform.position, transform.forward);
+        Ray back_ray = new Ray(transform.position, -transform.forward);
+        Ray right_ray = new Ray(transform.position, transform.right);
+
+        //Raycast left to see if I find a wall
+        if (Physics.Raycast(left_ray, out hit))
+        {
+            distance_to_wall_left = hit.distance;
+        }
+        else
+        {
+            distance_to_wall_left = 3;
+        }
+
+        //Raycast center forward to find a wall
+        if (Physics.Raycast(front_ray, out hit))
+        {
+            distance_to_wall_forward = hit.distance;
+        }
+        else
+        {
+            distance_to_wall_forward = 3;
+        }
+
+        //Raycast center forward to find a wall
+        if (Physics.Raycast(back_ray, out hit))
+        {
+            distance_to_wall_back = hit.distance;
+        }
+        else
+        {
+            distance_to_wall_back = 3;
+        }
+
+        //Raycast right to find a wall
+        if (Physics.Raycast(right_ray, out hit))
+        {
+            distance_to_wall_right = hit.distance;
+        }
+        else
+        {
+            distance_to_wall_right = 3;
+        }
+
+
+        //Raycast down to find the ground
+        if (Physics.Raycast(transform.position, -transform.up, 1.1f))
+        {
+            is_grounded = true;
+        }
+        else
+        {
+            is_grounded = false;
+        }
+
+    }
+}
---
+++ Group22 Game Project 180/Assets/Scripts/Player.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* Author: [Grant Stewart]
+ * Date:   [03-01-2022]
+ *         [This script will solve doors 2 lab by creating doors and keys]
+ */
+
+public class Player : MonoBehaviour
+{
+    public int lime_keys = 0;
+    public int blue_keys = 0;
+    public int red_keys = 0;
+    public int gold_keys = 0;
+    public int cyan_keys = 0;
+    public int purple_keys = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+ 
+        //picks up lime orb
+        if(other.gameObject.tag == "Lime Orb")
+        {
+            lime_keys++;
+            print("Orb Green " + lime_keys);
+            //print("Lime Orb - Tag you're it!");
+            other.gameObject.SetActive(false);
+        }
+
+        //unlocks lime door
+        if(other.gameObject.tag == "Lime Door")
+        {
+            print("The door is locked.");
+
+            if ((lime_keys >= other.gameObject.GetComponent<Door>().number_of_locks_lime) && (cyan_keys >= other.gameObject.GetComponent<Door>().number_of_locks_cyan) && (gold_keys >= other.gameObject.GetComponent<Door>().number_of_locks_gold))
+            {
+                print("Lime door unlocked!");
+                lime_keys -= other.gameObject.GetComponent<Door>().number_of_locks_lime;
+                cyan_keys -= other.gameObject.GetComponent<Door>().number_of_locks_cyan;
+                gold_keys -= other.gameObject.GetComponent<Door>().number_of_locks_gold;
+
+                other.gameObject.SetActive(false);
+            }
+        }
+
+        //picks up blue orb
+        if(other.gameObject.tag == "Blue Orb")
+        {
+            blue_keys++;
+            print("Orb Blue " + blue_keys);
+            //print("Blue Orb added!");
+            other.gameObject.SetActive(false);            
+        }
+
+        //unlocks blue door
+        if (other.gameObject.tag == "Blue Door")
+        {
+            print("The door is locked.");
+
+            if ((blue_keys >= other.gameObject.GetComponent<Door>().number_of_locks_blue) && (red_keys >= other.gameObject.GetComponent<Door>().number_of_locks_red) && (purple_keys >= other.gameObject.GetComponent<Door>().number_of_locks_purple))
+            //if x>=2
+            {
+                print("Blue door unlocked!");
+                blue_keys -= other.gameObject.GetComponent<Door>().number_of_locks_blue;
+                red_keys -= other.gameObject.GetComponent<Door>().number_of_locks_red;
+                purple_keys -= other.gameObject.GetComponent<Door>().number_of_locks_purple;
+                other.gameObject.SetActive(false);
+            }
+        }
+
+        //picks up red orb
+        if (other.gameObject.tag == "Red Orb")
+        {
+            red_keys++;
+            print("Orb Red " + red_keys);
+            //print("Red Orb added!");
+            other.gameObject.SetActive(false);
+        }
+
+        //unlocks red door
+        if (other.gameObject.tag == "Red Door")
+        {
+            print("The door is locked.");
+
+            if ((red_keys >= other.gameObject.GetComponent<Door>().number_of_locks_red) && (gold_keys >= other.gameObject.GetComponent<Door>().number_of_locks_gold))
+            //if x>=2
+            {
+                print("Red door unlocked!");
+                red_keys -= other.gameObject.GetComponent<Door>().number_of_locks_red;
+                gold_keys -= other.gameObject.GetComponent<Door>().number_of_locks_gold;
+                other.gameObject.SetActive(false);
+            }
+        }
+
+        //picks up gold orb
+        if (other.gameObject.tag == "Gold Orb")
+        {
+            gold_keys++;
+            print("Orb Gold " + gold_keys);
+            //print("Gold Orb added!");
+            other.gameObject.SetActive(false);
+        }
+
+        //picks up cyan orb
+        if (other.gameObject.tag == "Cyan Orb")
+        {
+            cyan_keys++;
+            print("Orb Cyan " + cyan_keys);
+            other.gameObject.SetActive(false);
+        }
+
+        //picks up purple orb
+        if (other.gameObject.tag == "Purple Orb")
+        {
+            purple_keys++;
+            print("Orb Purple " + purple_keys);
+            other.gameObject.SetActive(false);
+        }
+    }
+}  // end of Player Script
